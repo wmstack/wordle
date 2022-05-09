5fc7e5aa73c8861192a95c3b6d342d4c2de4d5f4
Waleed Dahshan <waleedmast@gmail.com>
2022-05-09 11:43:30 +1000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import wasm from "../src-wasm/Cargo.toml";
+	import {app, dialog} from "@tauri-apps/api";
+
+	let name: string;
+	app.getName().then((r)=> name = r)
+
+	async function load() {
+		let glob = await wasm({});
+		glob.hello();
+	}
+	
+	load();
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h1>{name}</h1>
+	<!-- <h1>{Tauri}</h1> -->
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
